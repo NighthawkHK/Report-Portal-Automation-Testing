06061857348e2402179f32511101e719b97f5d70
Maksym Vovk <Maksym_Vovk@epam.com>
2022-07-19 15:39:45 +0300

    Add Data provider for LoginTest

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,19 +3,24 @@ package tests;
 import entities.User;
 import listeners.TestListener;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 @Listeners(TestListener.class)
 public class LoginTest extends BaseTest {
 
-    private User testUser;
+    @DataProvider(name = "users")
+    public Object[][] dataProv() {
+        return new Object[][] {
+                {new User("default", "1q2w3e")}
+        };
+    }
 
-    @Test
-    public void verifyLoginIsSuccessful() {
-        testUser = new User("default", "1q2w3e");
+    @Test(dataProvider = "users")
+    public void verifyLoginIsSuccessful(User user) {
         Assert.assertEquals(allPages.getLoginPage()
-                .signIn(testUser)
+                .signIn(user)
                 .getPageTitle(), "ALL DASHBOARDS");
     }
 }
