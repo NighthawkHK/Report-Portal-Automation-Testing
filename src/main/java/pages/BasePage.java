080b13086aca35ae0549581e11b0297c0472e915
Maksym Vovk <Maksym_Vovk@epam.com>
2022-07-19 22:38:28 +0300

    Add logger for base page

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,17 +1,19 @@
 package pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import utils.PropertyReader;
 
 import java.time.Duration;
 
 public abstract class BasePage {
 
     protected final WebDriver driver;
+    protected final Logger log = LogManager.getLogger(this.getClass());
 
-    public BasePage(WebDriver driver) {
+    protected BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
     }
