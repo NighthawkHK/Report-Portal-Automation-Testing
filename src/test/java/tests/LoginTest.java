ad31b427ff01413b5623b536609c64ee1654710a
Maksym Vovk <Maksym_Vovk@epam.com>
2022-07-20 15:34:19 +0300

    Move Data Provider to the separate class

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,21 +3,13 @@ package tests;
 import entities.User;
 import listeners.TestListener;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 @Listeners(TestListener.class)
 public class LoginTest extends BaseTest {
 
-    @DataProvider(name = "users")
-    public Object[][] dataProv() {
-        return new Object[][] {
-                {new User("default", "1q2w3e")}
-        };
-    }
-
-    @Test(dataProvider = "users")
+    @Test(dataProvider = "users", dataProviderClass = tests.data.StaticProvider.class)
     public void verifyLoginIsSuccessful(User user) {
         Assert.assertEquals(allPages.getLoginPage()
                 .signIn(user)
