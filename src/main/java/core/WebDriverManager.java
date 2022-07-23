dfaec7e4289d42e53d7a6e5bb457e849c52284a0
Maksym Vovk <Maksym_Vovk@epam.com>
2022-07-23 22:50:43 +0300

    Move WebDriverManager to the core directory

--- src/main/java/core/WebDriverManager.java
+++ src/main/java/core/WebDriverManager.java
@@ -1,10 +1,11 @@
-package utils;
+package core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringDecorator;
+import utils.PropertyReader;
 
 import java.time.Duration;
 
