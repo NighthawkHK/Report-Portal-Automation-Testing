f00837b49ef56f4337f975a83ff2769fc68863ed
Maksym Vovk <Maksym_Vovk@epam.com>
2022-07-23 22:49:17 +0300

    Add log4j2 annotation

--- src/main/java/core/WebDriverEventHandler.java
+++ src/main/java/core/WebDriverEventHandler.java
@@ -1,43 +1,41 @@
-package utils;
+package core;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverListener;
 
+@Log4j2
 public class WebDriverEventHandler implements WebDriverListener {
-
-    private static final Logger logger = LogManager.getLogger(WebDriverEventHandler.class);
 
     @Override
     public void beforeFindElement(WebDriver driver, By locator) {
-        logger.info("Looking for web element: {}", locator);
+        log.info("Looking for web element: {}", locator);
     }
 
     @Override
     public void beforeTo(WebDriver.Navigation navigation, String url) {
-        logger.info("Navigating to {}", url);
+        log.info("Navigating to {}", url);
     }
 
     @Override
     public void afterSendKeys(WebElement element, CharSequence... keysToSend) {
-        logger.info("Successfully sent keys {} for web element {}", keysToSend, element);
+        log.info("Successfully sent keys {} for web element {}", keysToSend, element);
     }
 
     @Override
     public void afterClick(WebElement element) {
-        logger.info("Successfully clicked: {}", element);
+        log.info("Successfully clicked: {}", element);
     }
 
     @Override
     public void beforeQuit(WebDriver driver) {
-        logger.info("Trying to shut down driver: {}", driver);
+        log.info("Trying to shut down driver: {}", driver);
     }
 
     @Override
     public void afterQuit(WebDriver driver) {
-        logger.info("Driver has successfully turned off");
+        log.info("Driver has successfully turned off");
     }
 }
--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -1,43 +1,42 @@
 package listeners;
 
+import core.WebDriverManager;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.FileUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-import utils.WebDriverManager;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Log4j2
 public class TestListener implements ITestListener {
 
-    private static final Logger logger = LogManager.getLogger(TestListener.class);
     private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy HH-mm-ss");
 
     public void onTestStart(ITestResult result) {
-        logger.info("Starting Test: " + result.getName());
+        log.info("Starting Test: " + result.getName());
     }
 
     public void onTestSuccess(ITestResult result) {
-        logger.info("Test Passed: " + result.getName());
+        log.info("Test Passed: " + result.getName());
     }
 
     public void onTestFailure(ITestResult result) {
-        logger.info("Test Failed: " + result.getName());
+        log.info("Test Failed: " + result.getName());
         File srcFile = ((TakesScreenshot) WebDriverManager.getDriver()).getScreenshotAs(OutputType.FILE);
         try {
             FileUtils.copyFile(srcFile, new File("./screenshots-failure/" + dateFormatter.format(new Date()) + ".png"));
         } catch (IOException e) {
-            logger.error("Exception occurred while copying file: ", e);
+            log.error("Exception occurred while copying file: ", e);
         }
     }
 
     public void onTestSkipped(ITestResult result) {
-        logger.info("Test Skipped: " + result.getName());
+        log.info("Test Skipped: " + result.getName());
     }
 }
