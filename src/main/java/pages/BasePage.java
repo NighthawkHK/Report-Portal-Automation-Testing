cc63e2de56473921e08502790d1022fe8a49bc1a
Maksym Vovk <Maksym_Vovk@epam.com>
2022-07-23 22:51:31 +0300

    Update BasePage class

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,14 +1,10 @@
 package pages;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-
 public abstract class BasePage {
 
     protected final WebDriver driver;
-    protected final Logger log = LogManager.getLogger(this.getClass());
 
     protected BasePage(WebDriver driver) {
         this.driver = driver;
