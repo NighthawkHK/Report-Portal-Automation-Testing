763949a70554e242b031c976bb97dc9387ddfb50
Maksym Vovk <Maksym_Vovk@epam.com>
2022-07-23 22:47:21 +0300

    Delete AllPages class

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,21 +1,18 @@
 package tests;
 
+import core.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import pages.AllPages;
 import utils.PropertyReader;
-import utils.WebDriverManager;
 
 public abstract class BaseTest {
 
-    protected AllPages allPages;
     private static final String URL = PropertyReader.getProperty("url");
 
     @BeforeClass
     public void setUp() {
         WebDriver driver = WebDriverManager.getDriver();
-        allPages = new AllPages(driver);
         driver.navigate().to(URL);
     }
 
