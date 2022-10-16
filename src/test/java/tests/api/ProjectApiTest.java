6bb26fe7e933348c654ec8533c043ad1b6bbc3ec
Maksym Vovk <Maksym_Vovk@epam.com>
2022-10-17 00:01:49 +0300

    Implement JSON schema validator

--- src/test/java/tests/api/ProjectApiTest.java
+++ src/test/java/tests/api/ProjectApiTest.java
@@ -7,19 +7,23 @@ import org.apache.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 
 @Severity(SeverityLevel.CRITICAL)
 public class ProjectApiTest extends BaseApiServiceTest {
 
     private static final String PROJECT_NAME = "default_personal";
+    private static final String PROJECT_ENDPOINT = "/v1/project/list/{projectName}";
 
     @Test
     public void verifyProjectMandatoryInfo() {
         Project project = given().spec(requestSpec)
                 .pathParam("projectName", PROJECT_NAME)
                 .when()
-                .get("/v1/project/list/{projectName}")
+                .get(PROJECT_ENDPOINT)
                 .then()
                 .assertThat()
                 .statusCode(HttpStatus.SC_OK)
@@ -28,5 +32,17 @@ public class ProjectApiTest extends BaseApiServiceTest {
         Assert.assertEquals(project.getName(), PROJECT_NAME);
         Assert.assertEquals(project.getId(), 2);
         Assert.assertEquals(project.getEntryType(), "PERSONAL");
+    }
+
+    @Test
+    public void verifyProjectContract() {
+        given().spec(requestSpec)
+                .pathParam("projectName", PROJECT_NAME)
+                .when()
+                .get(PROJECT_ENDPOINT)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body(matchesJsonSchema(new File("src/test/java/tests/data/jsons/project_schema.json")));
     }
 }
