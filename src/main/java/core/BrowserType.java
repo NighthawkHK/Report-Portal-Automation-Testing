3cb9b9753647a80f6da5e28d6d6717944c9d0109
Maksym Vovk <Maksym_Vovk@epam.com>
2022-10-20 12:33:17 +0300

    Integrate framework with SauceLabs

---
+++ src/main/java/core/BrowserType.java
@@ -0,0 +1,8 @@
+package core;
+
+enum BrowserType {
+    CHROME,
+    EDGE,
+    FIREFOX,
+    REMOTE
+}
--- src/main/java/core/WebDriverManager.java
+++ src/main/java/core/WebDriverManager.java
@@ -2,13 +2,8 @@ package core;
 
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import utils.PropertyReader;
-
-import java.time.Duration;
 
 @Log4j2
 public class WebDriverManager {
@@ -21,8 +16,8 @@ public class WebDriverManager {
     public static WebDriver getDriver() {
         if (driver == null) {
             try {
-                WebDriverType driverType = WebDriverType.valueOf(PropertyReader.getProperty("browserType"));
-                driver = new EventFiringDecorator(new WebDriverEventHandler()).decorate(DriverFactory.initializeWebDriver(driverType));
+                BrowserType browser = BrowserType.valueOf(PropertyReader.getProperty("browserType"));
+                driver = new EventFiringDecorator(new WebDriverEventHandler()).decorate(WebDriverFactory.initializeWebDriver(browser));
             } catch (IllegalArgumentException e) {
                 log.error("Invalid value of enum " + PropertyReader.getProperty("browserType"), e);
                 throw new RuntimeException(e);
@@ -36,39 +31,5 @@ public class WebDriverManager {
             driver.quit();
             driver = null;
         }
-    }
-
-    private static class DriverFactory {
-
-        private DriverFactory() {
-        }
-
-        private static WebDriver initializeWebDriver(final WebDriverType driverType) {
-            WebDriver webDriver;
-            switch (driverType) {
-                case CHROME:
-                    io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
-                    webDriver = new ChromeDriver();
-                    break;
-                case FIREFOX:
-                    io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver().setup();
-                    webDriver = new FirefoxDriver();
-                    break;
-                case EDGE:
-                    io.github.bonigarcia.wdm.WebDriverManager.edgedriver().setup();
-                    webDriver = new EdgeDriver();
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unrecognized driver type: " + driverType);
-            }
-            webDriver.manage().window().maximize();
-            long implicitlyWait = Long.parseLong(PropertyReader.getProperty("implicitlyWait"));
-            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitlyWait));
-            return webDriver;
-        }
-    }
-
-    private enum WebDriverType {
-        CHROME, EDGE, FIREFOX
     }
 }
